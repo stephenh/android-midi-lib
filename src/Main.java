c15331546654d6646e2b38cab656adc986483850
Alex Leffelman <alex@remind101.com>
2014-03-07 12:44:18 -0800

    Adding success message;

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,8 @@ public class Main
         {
             mf = new MidiFile(input);
             mf.dumpToConsole();
+            
+            System.out.println("Success!");
         }
         catch(IOException e)
         {
